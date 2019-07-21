9e68f4a768b4c0f9ac5ee112b270d1bec679f8a9
leiqjl <leiqjl@gmail.com>
2019-07-21 14:49:34 +0800

    remove unuse

--- 2/2.79.c
+++ 2/2.79.c
@@ -1,19 +1,11 @@
 #include <assert.h>
 #include <limits.h>
-int divide_power2(int x, int k);
 int mul3div4(int x);
 int main(void)
 {
     int x = 0x87654321;
     assert(mul3div4(x) == x * 3 / 4);
     return 0;
-}
-
-int divide_power2(int x, int k)
-{
-    (x & INT_MIN) && (x = x + (1 << k) - 1);
-
-    return x >> k;
 }
 
 int mul3div4(int x)
